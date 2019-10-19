1974954e92ae59fb1d36c57056bd0b25af940fc0
JaredTomino <56745742+JaredTomino@users.noreply.github.com>
2019-10-19 10:19:22 +0800

    Add files via upload

---
+++ Initial.java
@@ -0,0 +1,9 @@
+public class Initial{
+  public static void main(String [] args){
+    System.out.println("     J FFFFFF");
+    System.out.println("     J F      ");
+    System.out.println("     J FFFF");
+    System.out.println("J    J F");
+    System.out.println("JJJJJJ F       ");
+     }
+} 
---
+++ MyInfo.java
@@ -0,0 +1,9 @@
+public class MyInfo{
+  public static void main(String [] args){
+    System.out.print("Tomino Jared S");
+    System.out.println("18");
+    System.out.println("Computer science");
+    System.out.println("Ampucao Proper, Itogon Benguet");
+    
+     }
+} 
